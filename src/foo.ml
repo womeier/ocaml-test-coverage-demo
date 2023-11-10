25b0939667a23a9f35ce7bdc888ccbcc349bba30
Wolfgang Meier <womeier@posteo.de>
2023-11-10 19:12:37 +0100

    simplify

--- src/foo.ml
+++ src/foo.ml
@@ -1,10 +1,3 @@
-let gen_hello_world () =
-  let output_str = "Hello World" in
-  print_string output_str ; (* insert print statement *)
-                            output_str
-
 (* Test in same file as source code *)
-let%expect_test "Test name" =
-  (* run src code - here I'm ignoring the output *)
-  ignore (gen_hello_world () : string) ;
-  [%expect {|Hello World|}]
+let%test "Test name" =
+  true
